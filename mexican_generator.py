fc8b70597f78707f61e8ce5b07f15aae624de1ca
cogitantium <falke.cs@gmail.com>
2020-10-20 15:00:16 +0200

    Add json serialising of state q111 transitions

--- mexican_generator.py
+++ mexican_generator.py
@@ -1,13 +1,19 @@
 import json
 from enum import Enum
 from pprint import pprint
+import collections.abc
 
 DEBUG = True
 
 
 def write_cgs(file, cgs):
+    def set_default(obj):
+        if isinstance(obj, set):
+            return list(obj)
+        raise TypeError
+
     with open(file, 'w+') as o:
-        o.writelines(json.dumps(cgs))
+        o.writelines(json.dumps(cgs, default=set_default))
 
 
 class CGS:
@@ -30,10 +36,26 @@ def kill_method(source, value, length):
     return (source + value) % length
 
 
-def generate_mexican():
+def update(d, u):
+    """
+    https://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth
+    """
+    for k, v in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+
+def generate_mexican(cgs: CGS):
+    # transitions[state][player1choice][player2choice][player3choice] -> new_state
     i = 0
     length = 3
     out = []
+    for state in range(0, 8):
+        cgs.game_struct['transitions'].update({state: {}})
+
     for p1_move in MexicanMoves:
         for p2_move in MexicanMoves:
             for p3_move in MexicanMoves:
@@ -51,7 +73,10 @@ def generate_mexican():
                 if DEBUG:
                     print(f"({i:2}) moves: {p1_move.value:2}, {p2_move.value:2}, {p3_move.value:2}, state: {state}")
 
-                out += [p1_move.value, p2_move.value, p3_move.value, state]
+                out += [[p1_move.value, p2_move.value, p3_move.value, state]]
+                print()
+                entry = {p1_move.value: {p2_move.value: {p3_move.value: {''.join(map(str, state))}}}}
+                cgs.game_struct['transitions'][7] = update(cgs.game_struct['transitions'][7], entry)
 
     return out
 
@@ -61,6 +86,9 @@ if __name__ == '__main__':
              1: [4, 5, 6],
              2: [7, 8, 9]}]
 
-    cgs = CGS()
-    pprint(generate_mexican())
-    write_cgs("test.json", test)
+    cgs = CGS(player_count=3)
+    gen = generate_mexican(cgs)
+    pprint(gen)
+
+    pprint(cgs.game_struct)
+    write_cgs("test.json", cgs.game_struct)
